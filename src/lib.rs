4c30972503a6de90730c78282b08ec7d930a25b4
tamaron <tamaron1203@gmail.com>
2022-03-27 15:06:08 +0900

    draw sky

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,17 @@
 mod utils;
 
-use nalgebra::*;
+use nalgebra::Vector3;
 use wasm_bindgen::prelude::*;
 use wasm_bindgen::JsCast;
 use web_sys::CanvasRenderingContext2d;
 
+const ASPECT_RATIO: f64 = 16.0 / 9.0;
 const WIDTH: u32 = 512;
-const HEIGHT: u32 = 512;
+const HEIGHT: u32 = (WIDTH as f64 / ASPECT_RATIO) as u32;
 const RESOLUTION: u32 = 16;
+
+// (r, g, b) = (x, y, z)
+type Color = Vector3<f64>;
 
 pub struct Info {
     progress: u32,
@@ -21,13 +25,6 @@ impl Info {
     pub fn update_progress(&mut self, x: u32, y: u32) {
         self.progress = (((y * WIDTH + x) as f32 / (WIDTH * HEIGHT) as f32) * 100.0) as u32;
     }
-}
-
-struct Color {
-    r: f64,
-    g: f64,
-    b: f64,
-    a: f64,
 }
 
 #[wasm_bindgen(start)]
@@ -48,9 +45,39 @@ pub fn start() -> Result<(), JsValue> {
     Ok(())
 }
 
+struct Ray {
+    origin: Vector3<f64>,
+    direction: Vector3<f64>,
+}
+
+impl Ray {
+    fn new(origin: Vector3<f64>, direction: Vector3<f64>) -> Self {
+        Ray { origin, direction }
+    }
+    fn at(&self, t: f64) -> Vector3<f64> {
+        self.origin + t * self.direction
+    }
+}
+
 fn draw(context: &CanvasRenderingContext2d) {
     let mut info = Info::new();
 
+    //
+    // Camera
+    //
+    let viewport_height: f64 = 2.0;
+    let viewport_width: f64 = ASPECT_RATIO * viewport_height;
+    let forcal_length: f64 = 1.0;
+
+    let origin = Vector3::new(0., 0., 0.);
+    let horizontal = Vector3::new(viewport_width, 0., 0.);
+    let vertical = Vector3::new(0., viewport_height, 0.);
+    let lower_left_corner =
+        origin - horizontal / 2.0 - vertical / 2.0 - Vector3::new(0., 0., forcal_length);
+
+    //
+    // Render
+    //
     context.save();
     for y in 0..HEIGHT {
         for x in 0..WIDTH {
@@ -62,23 +89,37 @@ fn draw(context: &CanvasRenderingContext2d) {
                 continue;
             }
 
-            let color = Color {
-                r: x as f64,
-                g: y as f64,
-                b: 0.25,
-                a: 1.0,
-            };
-            write_color(&context, x, y, color);
+            let u = x as f64 / (WIDTH - 1) as f64;
+            let v = y as f64 / (HEIGHT - 1) as f64;
+            let r = Ray::new(
+                origin,
+                lower_left_corner + u * horizontal + v * vertical - origin,
+            );
+            let pixel_color = ray_color(r);
+            write_color(&context, x, y, pixel_color);
         }
     }
     log!("Done!");
 }
 
+fn ray_color(r: Ray) -> Color {
+    let unit_direction = r.direction.normalize();
+    let t = 0.5 * (unit_direction.y + 1.0);
+    (1.0 - t) * Color::new(1.0, 1.0, 1.0) + t * Color::new(0.5, 0.7, 1.0)
+}
+
 fn write_color(context: &CanvasRenderingContext2d, x: u32, y: u32, color: Color) {
-    let Color { r, g, b, a } = color;
+    // a is hardcoded to 0
+    let (r, g, b, a) = (color.x, color.y, color.z, 1.0);
     let px = x as f64;
     let py = y as f64;
-    let color = JsValue::from_str(&format!("rgba({},{},{},{})", r, g, b, a));
+    let color = JsValue::from_str(&format!(
+        "rgba({},{},{},{})",
+        255. * r,
+        255. * g,
+        255. * b,
+        255. * a
+    ));
     context.set_fill_style(&color);
     context.fill_rect(px, py, px + RESOLUTION as f64, py + RESOLUTION as f64);
 }
